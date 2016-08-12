625c30a9c184fdfbe4d42de50115a8e2482b540c
Nathan Cerny <ncerny@chef.io>
2016-08-12 13:19:36 -0500

    Use version pinning in environments.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,6 @@
 
 node['sauron'].each do |env, apps|
   apps.each do |name, version|
-    execute "chef-client -o 'recipe[#{name}@#{version}]' -c /tmp/kitchen/client.rb --lockfile /tmp/#{env}.lock -E #{env}"
+    execute "chef-client -o 'recipe[#{name}]' -c /tmp/kitchen/client.rb --lockfile /tmp/#{env}.lock -E #{env}"
   end
 end
